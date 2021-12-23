81d81eeba82d179018750b1126972bf95ec441b0
rbaks <andri.bakoson@gmail.com>
2021-12-23 16:37:20 +0300

    hide unfinished

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,7 +1,7 @@
 const projectsData = [
   {
     title: 'Memebot',
-    description: `A Facebook Messenger chatbot that generates memes from messages people send.`,
+    description: `A Facebook Messenger chatbot that generates memes from messages people send via publicly available image APIs.`,
     imgSrc: '/static/images/memebot.jpg',
     href: 'http://m.me/115066520196547',
     hobby: true
